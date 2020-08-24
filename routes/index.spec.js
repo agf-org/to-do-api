744a18067b763496777efbdd329babd1ee2f2e31
Álvaro García Fernández <alvarogarciafer@gmail.com>
2020-08-24 20:01:26 +0200

    Add more unit tests to / endpoint

--- routes/index.spec.js
+++ routes/index.spec.js
@@ -2,10 +2,16 @@ const app = require('../app');
 const request = require('supertest');
 
 describe('/ endpoint', () => {
-  it('should return the list of items', async () => {
+  it('should return 200 and OK for get request method', async () => {
     const res = await request(app)
       .get('/');
     expect(res.statusCode).toEqual(200);
     expect(res.text).toEqual('OK');
+  })
+  
+  it('should return 404 for invalid request method', async () => {
+    const res = await request(app)
+      .post('/');
+    expect(res.statusCode).toEqual(404);
   })
 })
