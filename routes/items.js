0b956f037850fffae233dc2650626623416b1c1d
Álvaro García Fernández <alvarogarciafer@gmail.com>
2020-08-24 18:28:56 +0200

    Add GET /items endpoint

--- routes/items.js
+++ routes/items.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+const {items} = require('../data/data');
+
+/* GET items */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  res.send(items);
 });
 
 module.exports = router;
