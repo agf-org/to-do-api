254b9873b699823431930357be6df901405ec524
Álvaro García Fernández <alvarogarciafer@gmail.com>
2020-08-29 18:58:03 +0200

    Add swagger

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var itemsRouter = require('./routes/items');
+const indexRouter = require('./routes/index');
+const apiDocsRouter = require('./routes/api-docs');
+const itemsRouter = require('./routes/items');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,22 +16,19 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api-docs', apiDocsRouter);
 app.use('/items', itemsRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((request, response, next) => {
+  const error = new Error(`${request.method} ${request.url} Not Found`);
+  error.status = 404;
+  next(error);
 });
 
-// error handler
-app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use((error, request, response) => {
+  console.error(error);
+  response.status(error.status || 500);
+  response.json({error:{message:error.message}});
 });
 
 module.exports = app;
