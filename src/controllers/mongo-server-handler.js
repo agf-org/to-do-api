01d26fd01670a7c891f41b6569539e515cdc98df
Álvaro García Fernández <alvarogarciafer@gmail.com>
2020-10-12 15:31:48 +0200

    add environment variables for database

--- src/controllers/mongo-server-handler.js
+++ src/controllers/mongo-server-handler.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 module.exports.connect = async () => {
-  const mongoUri = `mongodb://mongo:27017/notebook`
+  const mongoUri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/notebook`
   const mongooseOpts = {
     useNewUrlParser: true,
     useUnifiedTopology: true
