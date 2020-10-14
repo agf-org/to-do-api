9e4299c21c91f0146c3bdf700a8cbff72bcd9f37
Álvaro García Fernández <alvarogarciafer@gmail.com>
2020-10-14 21:58:10 +0200

    rename environment variables

--- src/controllers/mongo-server-handler.js
+++ src/controllers/mongo-server-handler.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 module.exports.connect = async () => {
-  const mongoUri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/notebook`
+  const mongoUri = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/notebook`
   const mongooseOpts = {
     useNewUrlParser: true,
     useUnifiedTopology: true
